c8226b3c5596ea72a750b376a95e25eb72271e8f
Mohammed Subani <63209210+mohammedSubani@users.noreply.github.com>
2022-11-25 10:51:06 +0300

    Add files via upload

---
+++ Central Server Application/Models/Account.cs
@@ -0,0 +1,33 @@
+﻿/**
+Project: BEMS IoT Enabled System V 1.0.0
+File Name: Account.cs
+Development Environment: Microsoft Visual Studio v16.8.4 Community Version
+Code contributors: Mohammed Ahmed Subani
+ASU ID: 1218278997
+
+File Description:
+This is the model for accounts. This contains the information that user might have.
+This implementation can be used by the AccountController.
+
+Old Functions:None
+New Functions:None
+Modified Functions:None
+*/
+
+using EncryptDecrypt;
+
+namespace WebApplication1.Models
+{
+    public class Account
+    {
+        /*Data fields for Account*/
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string role { get; set; }
+        public string phoneNumber { get; set; }
+        public string address { get; set; }
+        public string zipCode { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+}
---
+++ Central Server Application/Models/CurrentWeather.cs
@@ -0,0 +1,61 @@
+﻿/**
+Project: BEMS IoT Enabled System V 1.0.0
+File Name: CurrentWeather.cs
+Development Environment: Microsoft Visual Studio v16.8.4 Community Version
+Code contributors: Mohammed Ahmed Subani
+ASU ID: 1218278997
+
+File Description:
+This is the model for weather api service instance creation,call and data processing.
+The instance for this class creates service client to be called by the controllers in the
+MVC web application.
+
+Old Functions:None
+New Functions:CallService.
+Modified Functions:None
+*/
+
+namespace WebApplication1.Models
+{
+    public class CurrentWeather
+    {
+        public string Humidity = "";    //Current humidity data field 
+        public string CloudAmount = ""; //Current cloud amounts data field 
+        public string temp = "";        //Current temprature data field 
+        public string WindSpeed = "";   //Current wind speed data field 
+        public string WindrDir = "";    //Current wind direction data field   
+
+        public string condition = "";   //Current weather condition data field 
+        public string icon = "";        //Current weather condition icon URL data field 
+
+        /*The service client instance creation for the weather service*/
+        public WeatherService.ServiceClient WeatherCaller = new WeatherService.ServiceClient();
+
+
+        /**
+            Function Description:
+                The following function call the weather service at the system's location and 
+                upon sucessful data retrieval assigns the data fileds of weather data in this
+                class.
+
+            @Parameters: void
+            @Returns: void
+        */
+        public void CallService() 
+        {
+            /*Calling service at system's location (latitude,longitude)*/
+            WeatherCaller.GetWeatherJSON(31.9702593, 35.9568498);
+
+            /*Upon sucessful retrieval assign data fields */
+            if (WeatherCaller.SucessRetrieval()) 
+            {
+                Humidity = WeatherCaller.GetHum();
+                CloudAmount = WeatherCaller.GetCloudAmnt();
+                WindSpeed = WeatherCaller.GetCloudAmnt();
+                WindrDir = WeatherCaller.GetWndDir();
+                temp = WeatherCaller.GetTemp();
+            }
+        }
+
+    }
+}
