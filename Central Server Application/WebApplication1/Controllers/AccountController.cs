61d57689fd98ef73fdf17fdc6f020df65c7d8aa1
Mohammed Subani <63209210+mohammedSubani@users.noreply.github.com>
2022-11-25 10:56:18 +0300

    Add files via upload

---
+++ Central Server Application/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,301 @@
+﻿/**
+Project: BEMS IoT Enabled System V 1.0.0
+File Name: TestController.cs
+Development Environment: Microsoft Visual Studio v16.8.4 Community Version
+Code contributors: Mohammed Ahmed Subani
+ASU ID: 1218278997
+
+File Description:
+This is the controller for the Account management component, this implementation
+determines the logic for creating authorization and authentication functionality
+this solution uses a cookie-based authentication and authorization using AspNetCore
+libraries for authentication and authorization.
+
+Old Functions:None
+
+New Functions:
+
+Modified Functions:None
+*/
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using EncryptDecrypt;
+using WebApplication1.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net.Sockets;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        /*Model class to call Account Management Service to apply users database logic*/
+        public AccountMgmt AccountMgmtSvc = new AccountMgmt();
+
+        /**
+            Function Description:
+                The following function returns the homepage view.
+
+            @Parameters: void
+            @Returns: IActionResult: View of homepage
+        */
+        public IActionResult Index()
+        {
+            return View();
+        }
+         
+        /**
+             Function Description:
+                 The following function returns the Login view. This view contains 
+                 input form with username and password fields. non-authenticated
+                 users will be re-directed to this page when they try to access pages
+                 with authentication restrictions.
+
+             @Parameters: void
+             @Returns: IActionResult: View of Login
+        */
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /**
+              Function Description:
+                  The following function returns the AccessDenied view. This view 
+                  is used to redirect to when the user is not authorized.
+
+              @Parameters: void
+              @Returns: IActionResult: View of AccessDenied
+         */
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        /**
+             Function Description:
+                 The following function returns the Homepage view. after successful logging in.
+                 in case of unsucessful login, login page view is returned with messages 
+                 notifying the user for the reason of login failuare.
+
+                 ** Cookies are non-presistant after a period of time of inactivity users will 
+                 *  need to login again.
+                
+             @Parameters: string userName, string password
+             @Returns: IActionResult: View of Login / View of Homepage
+        */
+        [HttpPost]
+        public IActionResult Login(string userName, string password)
+        {
+            /*If username or password fileds submit is empty*/
+            if (userName == null || password == null || userName == "" || password == "")
+            {
+                ViewBag.ErrorMessage = "*Empty fields";
+                return View();
+            }
+
+            /*Encrypting password to be used later with Account Management Service 
+              Authentication function*/
+            try { password = EncryptDecryprt.EncryptString(password); }
+            catch (Exception) { ViewBag.ErrorMessage = "Encryption/decryption error"; }
+
+            /*Creating Identity object for MVC authentication and authorization*/
+            ClaimsIdentity identity = null;
+            string role = null;
+
+            /*If Account Management Service returned true result for authentication
+              set up Idenity role by calling Auhtenticate function in the 
+              Account Management Service*/
+            if (AccountMgmtSvc.Authenticate(userName, password))
+                role = AccountMgmtSvc.Authorize(userName);
+            else
+            {
+                /* In case of failed authentication notify user*/
+                ViewBag.ErrorMessage = "Invalid username or password";
+                return View();
+            }
+
+            /*At this point authentication has happened sucessfully configur Identity object
+              for cookie-based authentication(setting username,role and type of authentication)*/
+
+            identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Role, role)
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            /*Applying the Identity object settings*/
+            var principal = new ClaimsPrincipal(identity);
+
+            /*Starting the authenticated cookie-based session*/
+            var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+            /*Return to homepage*/
+            return RedirectToAction("Index", "Home");
+        }
+
+        /**
+             Function Description:
+                 The following function is called upon loggin out using the logout button
+                 that appears on main navigation bar after sucessful login. Calling this
+                 function ends 
+                
+             @Parameters: string userName, string password
+             @Returns: IActionResult: View of Login / View of Homepage
+        */
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            /*Logging out and redirect to homepage*/
+            var login = HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+        /**
+            Function Description:
+                The following function returns the Register view. the register view contains
+                a form for registration with the required fields.
+
+                @Parameters: void
+                @Returns: IActionResult: View of Register with registration form
+        */
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        /**
+             Function Description:
+                 The following function is called after submitting the Register forms. If the registration
+                 is sucessful user will recieve a notifying email, user will be logged in and re-directed to
+                 homepage. In case of failed registration Register view is returned with notifying message
+                 for the reason of failing to register.
+
+                 ** Cookies are non-presistant after a period of time of inactivity users will 
+                 *  need to login again.
+                
+             @Parameters: string userName, string password, string re_password, string email, string phoneNumber
+             @Returns: IActionResult: View of Register / View of Homepage
+        */
+        [HttpPost]
+        public IActionResult Register(string userName, string password, string re_password, string email, string phoneNumber)
+        {
+            /*Guard against empty fields*/
+            if (phoneNumber == "" || phoneNumber == null)
+            {
+                ViewBag.ErrorMessage2 = "* No phone number entered";
+                return View();
+            }
+
+            /*Remove white spaces*/
+            phoneNumber = phoneNumber.Trim();
+
+            /*Check for phone number format*/
+            if (!Int32.TryParse(phoneNumber, out int num))
+            {
+                ViewBag.ErrorMessage2 = "* Bad phone number format";
+                return View();
+            }
+
+            /*Check for matching password fields*/
+            if (password != re_password)
+            {
+                ViewBag.ErrorMessage2 = "* Passwords didn't match !";
+                return View();
+            }
+
+            /*
+              If passwords matched encrypt the password for registration and loggin in.
+              By this logic users database only registers encrypted passwords.
+            */
+
+            try { password = EncryptDecryprt.EncryptString(password); }
+            catch (Exception)
+            {
+                ViewBag.ErrorMessage2 = "* REGISTER IS UNSCUCESSFUL ! Encryption/decryption error";
+                return View();
+            }
+
+            /* 
+             Registering the new user , if registration is successful send an email to the
+             user, user is logged in and redirected to homepage. Otherwise the user is notified 
+             for the reason of failing to register.
+            */
+            if (AccountMgmtSvc.Register(userName, password, email, phoneNumber))
+            {
+                /*Trying to send email */
+                try { Execute(email).Wait(); }
+                catch (Exception)
+                {/*Failing to send email catch the exception*/}
+
+                /*Creating Identity object for MVC authentication and authorization*/
+                ClaimsIdentity identity = null;
+
+                /*Setting the role to user, admins registration is done manually editing the 
+                 role part in the users database file*/
+                string role = "user";
+
+                /*
+                 At this point registration and authentication has happened sucessfully configure 
+                 Identity object for cookie-based authentication
+                 (setting username,role and type of authentication)
+                */
+
+                identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Role, role)
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                /*Applying the Identity object settings*/
+                var principal = new ClaimsPrincipal(identity);
+
+                /*Starting the authenticated cookie-based session*/
+                var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                /*Redirect to homepage*/
+                return RedirectToAction("Index", "Home");
+            }
+            else
+                ViewBag.ErrorMessage2 = "* REGISTER IS UNSCUCESSFUL! , user name or email already exists !";
+
+            return View();
+        }
+
+        /**
+            Function Description:
+                 The following function is an implementation for Microsoft SendGrid API
+                 this function implementation is orginally found on Microsoft SendAPI 
+                 platform at: https://app.sendgrid.com/guide/integrate/langs/csharp
+                    
+                 The implementation is modified to meet BEMS IoT Enabled System requirements.
+
+                @Parameters: void
+                @Returns: Task completed object for sending email via SendGrid API
+        */
+        private static async Task Execute(string email)
+        {
+            try
+            {
+                var apiKey = "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";
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress("deltan1ms@live.com", "BEMS_IoT Admin");
+                var subject = "BEMS_IoT_Enabled Registeration";
+                var to = new EmailAddress(email, "user");
+                var plainTextContent = "Thank you for regisetring in BEMS_IoT_Enabled project";
+                var htmlContent = "<strong>Thank you for regisetring in BEMS_IoT_Enabled project</strong>";
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+            }
+
+            catch (Exception) {/*Do Nothing*/}
+        }
+
+
+    }
+}
---
+++ Central Server Application/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,393 @@
+﻿/**
+Project: BEMS IoT Enabled System V 1.0.0
+File Name: HomeController.cs
+Development Environment: Microsoft Visual Studio v16.8.4 Community Version
+Code contributors: Mohammed Ahmed Subani
+ASU ID: 1218278997
+
+File Description:
+This is the controller for the BEMS IoT components, this implementation
+determines how the website main pages views processing is done.
+
+Old Functions:None
+
+New Functions:
+    Index, Dashboards, RawData, IndoorParam, Analytics, OutdoorParam, AggregateData, 
+    SensingUnits, Controlling, Contact, TestSuite, About.
+
+Modified Functions:None
+*/
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using System.Net;
+using System.Xml;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        /*CurrentWeather model class instance for calling weather service*/
+        public CurrentWeather OutdoorWeather = new CurrentWeather();
+        
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        /**
+            Function Description:
+                The following function returns the homepage view.
+
+            @Parameters: void
+            @Returns: IActionResult: View of homepage
+        */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /**
+         Function Description:
+            The following function returns the Dashboards view.
+
+            @Parameters: void
+            @Returns: IActionResult: View of Dashboards
+        */
+        [Authorize]
+        public IActionResult Dashboards()
+        {
+            return View();
+        }
+
+        /**
+         Function Description:
+            The following function returns the RawData view.
+
+            @Parameters: void
+            @Returns: IActionResult: View of RawData
+        */
+        [Authorize(Roles ="Admin")]
+        public IActionResult RawData()
+        {
+            return View();
+        }
+
+        /**
+         Function Description:
+            The following function returns the IndoorParam view.
+
+            @Parameters: void
+            @Returns: IActionResult: View of IndoorParam
+        */
+        [Authorize]
+        public IActionResult IndoorParam()
+        {
+            return View();
+        }
+
+        /**
+         Function Description:
+            The following function returns the Analytics view.
+
+            @Parameters: void
+            @Returns: IActionResult: View of Analytics
+        */
+        [Authorize(Roles = "Admin")]
+        public IActionResult Analytics()
+        {
+            return View();
+        }
+
+        /**
+         Function Description:
+            The following function returns the OutdoorParam view.
+            Upon page loading this function will use the CurrentWeather class
+            instance to create Weather Service Instance and make RESTful calls
+            to other climate related services using RESTful calls.
+
+            @Parameters: void
+            @Returns: IActionResult: View of OutdoorParam with calling to weather services
+        */
+        [Authorize]
+        public IActionResult OutdoorParam()
+        {
+            /*Use the CurrentWeather instace to get results from weather service*/
+            OutdoorWeather.CallService();
+
+            /*Get the messages to the view*/
+            ViewBag.WindDirection = OutdoorWeather.WindrDir;
+            ViewBag.WindSpeed = OutdoorWeather.WindSpeed;
+            ViewBag.Humidity = OutdoorWeather.Humidity;
+            ViewBag.Temp = OutdoorWeather.temp;
+            ViewBag.CloudAmount = OutdoorWeather.CloudAmount;
+
+            /*Calling a RESTful service and getting the elevation data related.*/
+            XmlDocument elevationDoc = new XmlDocument();
+
+            using (WebClient client = new WebClient())
+            {
+                /*RESTful URL with latitude/longitude parameters for the system's location*/
+                string urlString =
+                   "http://localhost:25253/pointElevation/Service.svc/findElevation?" +
+                   "latitude=31.9704752" +
+                   "&longitude=35.9568352";
+
+                /*RESTful call to the point elevation RESTful service*/
+                try { elevationDoc.LoadXml(client.DownloadString(urlString)); }
+
+                catch (WebException) {/*Failing to make a call*/ }
+            }
+
+            XmlNodeList elemList = elevationDoc.GetElementsByTagName("string");
+            string elevation = elemList[0].InnerXml;
+
+            ViewBag.PointElevation = elevation; /*returning the results*/
+
+            /*Calling a RESTful service SolarEnergy and getting the solar irradiance data.*/
+            using (WebClient client = new WebClient())
+            {
+                /*RESTful URL with latitude/longitude parameters for the system's location*/
+                string urlString =
+                   "http://localhost:25253/SolarEnergy/Service.svc/GetSolarIndex?" +
+                   "latitude=31.9704752" +
+                   "&longitude=35.9568352";
+
+                try 
+                {
+                    /*RESTful call to the service*/
+                    string SolarIrradiance = client.DownloadString(urlString);
+                    ViewBag.SolarIrradiance= SolarIrradiance.Trim(new Char[] {'\"'}); 
+                }
+
+                catch (WebException) {/*Failing to make a call*/  }
+            }
+
+            return View();
+        }
+
+        /**
+          Function Description:
+             The following function returns the AggregateData view.
+
+             @Parameters: void
+             @Returns: IActionResult: View of AggregateData
+         */
+        [Authorize]
+        public IActionResult AggregateData()
+        {
+            return View();
+        }
+
+        /**
+          Function Description:
+             The following function returns the SensingUnits view. upon page load
+             this controller will check for the health of system's attached hardware
+             units on local network assigned IP's and their assigned ports.
+
+             @Parameters: void
+             @Returns: IActionResult: View of SensingUnits with their status
+         */
+        [Authorize(Roles = "Admin")]
+        public IActionResult SensingUnits()
+        {
+            /*RESTful call to sesning unit 0 on port 5007*/
+            using (WebClient client = new WebClient())
+            {
+                /*Sensing unit URL of IP address and the assigned port 5007*/
+                string urlString = "http://192.168.1.101:5007/";
+
+                try
+                {
+                    /*
+                      RESTful call upon sucessful call set message sensingUnit_0 to be 
+                      interpreted by View code to decide how to present results
+                    */
+                    client.DownloadString(urlString);
+                    ViewBag.sensingUnit_0 = "true";
+                }
+
+                catch (WebException) 
+                {
+                    /*Failing to call the sensing unit*/
+                    ViewBag.sensingUnit_0 = "false";
+                    return View();
+                }
+            }
+
+            /*RESTful call to sesning unit 1 on port 5004*/
+            using (WebClient client = new WebClient())
+            {
+                /*Sensing unit URL of IP address and the assigned port 5004*/
+                string urlString = "http://192.168.1.102:5004/";
+
+
+                try
+                {
+                    /*
+                      RESTful call upon sucessful call set message sensingUnit_1 to be 
+                      interpreted by View code to decide how to present results
+                    */
+                    client.DownloadString(urlString);
+                    ViewBag.sensingUnit_1 = "true";
+                }
+
+                catch (WebException)
+                {
+                    /*Failing to call the sensing unit*/
+                    ViewBag.sensingUnit_1 = "false";
+                    return View();
+                }
+            }
+
+            /*RESTful call to control unit on port 5006*/
+            using (WebClient client = new WebClient())
+            {
+                string urlString = "http://192.168.1.103:5006/";
+
+
+                try
+                {
+                    /*
+                       RESTful call upon sucessful call set message sensingUnit_2 to be 
+                       interpreted by View code to decide how to present results
+                    */
+                    client.DownloadString(urlString);
+                    ViewBag.sensingUnit_2 = "true";
+                }
+
+                catch (WebException)
+                {
+                    /*Failing to call the sensing unit*/
+                    ViewBag.sensingUnit_2 = "false";
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+
+        /**
+         Function Description:
+            The following function returns the SensingUnits view. upon page submission
+            this controller will check for the health of system's attached hardware
+            units on local network assigned IP's and their assigned ports.
+
+            @Parameters: string str
+            @Returns: IActionResult: View of SensingUnits with their status
+        */
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult SensingUnits(string str)
+        {
+            return View();
+        }
+
+
+        /**
+         Function Description:
+            The following function returns controlling dashboard view for the control
+            module. The view contains a form to submit the switch to use.
+
+            @Parameters: void
+            @Returns: IActionResult: View of Controlling
+        */
+        [Authorize]
+        public IActionResult Controlling()
+        {
+            return View();
+        }
+
+
+        /**
+         Function Description:
+            The following function returns controlling dashboard view for the control
+            module. The view contains a form to submit the switch to use.
+
+            @Parameters: string switchRelay
+            @Returns: IActionResult: View of Controlling with status of assigned switch used
+        */
+        [Authorize]
+        [HttpPost]
+        public IActionResult Controlling(string switchRelay)
+        {
+            /*RESTful call to the control module service*/
+            using (WebClient client = new WebClient())
+            {
+                /*RESTful URL with switching the control module parameters*/
+                string urlString =
+                   "http://192.168.1.103:5006/relay"+ switchRelay;
+
+                /*RESTful call*/
+                try  
+                { ViewBag.Response += client.DownloadString(urlString); }
+
+                /*Failing to make RESTful call*/
+                catch (WebException) 
+                { ViewBag.ErrMsg = "Failed to switch relay number: " + switchRelay; }
+            }
+
+            return View();
+        }
+
+        /**
+          Function Description:
+             The following function returns the Contact view.
+
+             @Parameters: void
+             @Returns: IActionResult: View of Contact
+         */
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        /**
+          Function Description:
+             The following function returns the TestSuite component view. The links 
+             at this page uses TestController instead of main controller(HomeController).
+
+             @Parameters: void
+             @Returns: IActionResult: View of TestSuite
+         */
+        [Authorize(Roles = "Admin")]
+        public IActionResult TestSuite()
+        {
+
+            return View();
+        }
+
+        /**
+          Function Description:
+             The following function returns the About view.
+
+             @Parameters: void
+             @Returns: IActionResult: View of About
+         */
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
